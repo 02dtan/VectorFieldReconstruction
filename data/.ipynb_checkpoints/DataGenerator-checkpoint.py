bf65fb8e0915a6d603f9ca56fd155985f41a977a
Daniel Tan <dtan@daniels-mbp.rockefeller.edu>
2025-10-03 12:27:54 -0400

    initial DataGenerator and maps classes

---
+++ data/.ipynb_checkpoints/DataGenerator-checkpoint.py
@@ -0,0 +1,74 @@
+######
+# 
+# DT 10-03-2025
+# 
+# Define DataGenerator class to unify instances of map function and its parameters.
+# Each initialized DataGenerator represents a single map function for maps defined in data.maps,
+# and carries along with it a static parameter set for the map function (passed at initialization).
+# 
+# Use DataGenerator.generate_next() to advance a passed state Xn *one time*, using the parameterized map function.
+#     returns Xn+1, of dimensions Xn.shape.
+# 
+# Use DataGenerator.generate_series() to advance an initial state X0 $num_steps times, using the parameterized map function.
+#     returns X, a (num_steps * X0.shape) np.ndarray with axis 0 iterating over timesteps i \in num_steps, and all other axes
+#                representing the dimension of the state at timestep i. 
+# 
+######
+
+import numpy as np
+import numba as nb
+from typing import Callable, Any
+from data import maps
+
+class DataGenerator(object):
+
+    def __init__(self, X0: float | np.ndarray = 0.1,
+                 num_steps: int = 100,
+                 mapname: str = "anosov_diffeo",
+                 map_args: Any = None):
+
+        self.X0 = np.array(X0)
+            
+        self.num_steps = num_steps
+        
+        try:
+            self.map = getattr(maps, mapname)
+        except AttributeError:
+            print(f'Name {map} is not defined in maps.py!')
+
+        self.map_args = map_args
+
+    @staticmethod
+    @nb.njit(fastmath=True)
+    def evolve(Xn: float | np.ndarray,
+               mapfn: Callable[[Any], Any],
+               map_args: Any):
+        
+        return mapfn(Xn, map_args)
+
+    @staticmethod
+    @nb.njit(fastmath=True)
+    def evolve_all(X0: np.ndarray,              # Initial condition. If X0 passed as float to constructor, it is converted to (1,) np.ndarray. 
+                   X: np.ndarray,               # Empty, but initialized, data array.
+                   num_steps: int,              # Number of steps to iterate with dyanmics governed by mapfn.
+                   mapfn: Callable[[Any], Any], # The map used to advance states in time, which is an attribute of data.maps.
+                   map_args: Any):              # Any additional arguments are passed to mapfn.
+        
+        # Create data array of dimensions ( num_steps * state_dimensions ): 0 axis indexes over timesteps.
+        X[0] = X0
+        for i in range(num_steps - 1):
+            X[i+1] = mapfn(X[i], map_args)
+
+        return X
+        
+    # External interfaces
+    def generate_next(self, Xn: np.ndarray):
+        return self.evolve(Xn, self.map, self.map_args)
+    
+    def generate_series(self):
+        X0 = self.X0
+        num_steps = self.num_steps
+        X = np.empty((num_steps, *X0.shape))
+        
+        return self.evolve_all(X0, X, num_steps, self.map, self.map_args)
+        
---
+++ data/maps.py
@@ -0,0 +1,65 @@
+######
+# 
+# DT 10-03-2025
+# 
+# Define governing equations for various dynamical systems.
+# Import this to use definitions standalone, or initialize DataGenerator object with
+# argument map: str = $NAME_OF_FUNCTION and optional argument map_args: Any = {parameter set for map}
+# to generate time-series data whose evolution is governed by the equation given in NAME_OF_FUNCTION().
+# If map_args is not passed, the default parameter value(s) will be taken, e.g., \mu = 2 for tentmap,
+# or r = 4 for logisticmap. 
+# 
+######
+
+import numpy as np
+import numba as nb
+from typing import Any
+
+### Define the equation governing the time evolution of 1D states under tent map.
+# 
+# args: x_n, scalar \in [0,1]
+#       mu scalar
+#
+# returns: scalar f(x) = x_(n+1)
+# 
+@nb.njit(fastmath=True)
+def tentmap(x: float | np.ndarray, mu: Any):
+    
+    # Set default value
+    if mu is None:
+        mu = 1
+
+    return mu * np.min(np.array([x, 1-x]))
+
+### Define the equation governing the time evolution of 1D states under logistic map.
+# 
+# args: x_n, scalar \in [0,1] or vector with elements \in [0,1]
+#       r scalar
+# 
+# returns: scalar f(x) = x_(n+1), or vector f(X) = X_(n+1)
+# 
+@nb.njit(fastmath=True)
+def logisticmap(x: float | np.ndarray, r: Any):
+
+    # Set default value
+    if r is None:
+        r = 4
+        
+    return r * x * (1-x)
+
+### Define the equation governing the time evolution of 2D states in the Anosov diffeomorphism.
+# 
+# args: X_n, 2D vector representing cartesian (x, y) with elements in [0, 1)
+#       A, linear operator on X_n which must satisfy det(A) == 1
+#
+# returns: 2D vector f(X) = X_(n+1)
+# 
+@nb.njit(fastmath=True)
+def anosov_diffeo(X: np.ndarray, A: Any):
+    
+    # Set default value
+    if A is None:
+        A = np.array([[2,1],[1,1]])
+        
+    assert np.linalg.det(A) == 1
+    return (A@X)%1
